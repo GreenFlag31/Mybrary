7e5634be5fca77904d4c2dc96cf12cc79d8abbb5
MC <unam31@gmail.com>
2022-06-21 18:49:31 +0200

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 
 const indexRouter = require("./routes/index")
+const authorRouter = require("./routes/authors")
 
 app.set("view engine", "ejs")
 app.set("views", __dirname + "/views")
@@ -16,6 +18,8 @@ app.set("views", __dirname + "/views")
 app.set("layout", "layouts/layout")
 app.use(expressLayouts)
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }))
+
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
@@ -23,6 +27,7 @@ db.on("error", error => console.log(error))
 db.once("open", () => console.log("connected"))
 
 app.use("/", indexRouter)
+app.use("/authors", authorRouter)
 
 // routes = controller
 app.listen(process.env.PORT || 3000)
